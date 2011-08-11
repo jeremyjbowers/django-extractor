18e94408457b76a6a55892037a4f99f26f0b380e
Jeremy Bowers <jeremyjbowers@gmail.com>
2011-08-11 16:24:45 -0400

    Updates.

--- extractor/config/base/urls.py
+++ extractor/config/base/urls.py
@@ -6,5 +6,7 @@ from feed.api import v1
 urlpatterns = patterns('',
     url(r'^extractor/api/', include(v1.urls)),    
     url(r'^extractor/grappelli/', include('grappelli.urls')),
+    url(r'^extractor/admin/chronograph/job/(?P<pk>\d+)/run/$',
+        'chronograph.views.job_run', name="chronograph_job_run"),
     url(r'^extractor/admin/', include(admin.site.urls)),
 )
